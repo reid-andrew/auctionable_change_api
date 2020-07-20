28910667cd2d68cb8860f5cd3a25bdc5e9b11ce9
Andrew Reid <areid.sj@gmail.com>
2020-07-20 16:38:44 -0600

    Refactor items controller and endpoints

--- app/controllers/items.py
+++ app/controllers/items.py
@@ -1,21 +1,47 @@
 from app import app, db
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource, reqparse, request
+from flask_restful import fields, marshal_with, marshal
 from app.models.item import Item
 
-class ItemRoutes:
-    @app.route("/items")
-    def get_items():
-        try:
-            items=Item.query.all()
-            return jsonify([e.serialize() for e in items])
-        except Exception as e:
-            return(str(e))
+item_fields = {
+    'id': fields.Integer,
+    'title': fields.String,
+    'description': fields.String,
+    'price': fields.Float,
+    'donor': fields.String,
+    'status': fields.String,
+    'category': fields.String,
+    'charity': fields.String,
+    'image': fields.String
+}
 
-    @app.route("/items/<id_>")
-    def get_item(id_):
-        try:
-            item=Item.query.filter_by(id=id_).first()
-            return jsonify(item.serialize())
-        except Exception as e:
-            return(str(e))
+item_list_fields = {
+    'count': fields.Integer,
+    'items': fields.List(fields.Nested(item_fields))
+}
+
+item_post_parser = reqparse.RequestParser()
+
+class ItemResources(Resource):
+    def get(self, item_id=None):
+        if item_id:
+            item = Item.query.filter_by(id=item_id).first()
+            return marshal(item, item_list_fields)
+        else:
+            items = Item.query.all()
+            return marshal({
+                'count': len(items),
+                'items': [marshal(i, item_fields) for i in items]
+            }, item_list_fields)
+
+    @marshal_with(item_fields)
+    def post(self):
+        args = item_post_parser.parse_args()
+        
+        item = Item(**args)
+        db.session.add(item)
+        db.session.commit()
+
+        return item
