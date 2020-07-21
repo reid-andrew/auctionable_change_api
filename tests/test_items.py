5bdb14c87df5bdd6e1551519e26474d98c7f1087
Stephanie Friend <stephaniefriend91@gmail.com>
2020-07-20 21:01:37 -0600

    Wrote items test

--- tests/test_items.py
+++ tests/test_items.py
@@ -1,4 +1,26 @@
 import os
 import pytest
 
+from tests.support.configure_test import app
+from application import db
 
+import application.models.item as item
+from config import TestingConfig
+
+
+def test_db_create(app):
+    app = app(TestingConfig)
+
+    test_model_to_insert = item.Item(
+        description="Vintage wood rocking chair",
+        donor="Demo McDemoFace",
+        price=40.00,
+        status="For Sale",
+        title="Rocking Chair",
+        category="Big Cat Rescue",
+        image="img.ul"
+    )
+    test_model_to_insert.save()
+    db.session.commit()
+
+    assert db.session.query(item.Item).one()
