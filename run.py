ea4cb801d465274cf38c2fcc3cd08040a89d8a40
Stephanie Friend <stephaniefriend91@gmail.com>
2020-07-21 18:23:45 -0600

    Update run.py and procfile for Heroku

--- run.py
+++ run.py
@@ -8,4 +8,5 @@ config_name = os.getenv('APP_SETTINGS')
 app = create_app(config_name)
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
