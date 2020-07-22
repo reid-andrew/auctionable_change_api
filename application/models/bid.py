5ea9460b7146fbaf09609152b7c9e775895d9f44
Stephanie Friend <stephaniefriend91@gmail.com>
2020-07-21 21:36:41 -0600

    Created a bids table

---
+++ application/models/bid.py
@@ -0,0 +1,18 @@
+from application import db
+
+
+class Bid(db.Model):
+    __tablename__ = 'bids'
+
+    id = db.Column(db.Integer, primary_key=True)
+    bidder_name = db.Column(db.String())
+    bidder_email = db.Column(db.String())
+    amount = db.Column(db.Float())
+    street_address = db.Column(db.String())
+    city = db.Column(db.String())
+    state = db.Column(db.String())
+    zip_code = db.Column(db.String())
+    receipt = db.Column(db.String())
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
